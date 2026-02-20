16d58b76eb0b646f3f818398ec1355f04d9525cb
unknown <nascimentob.eliel@gmail.com>
2026-02-19 21:53:57 -0300

    repositorio aluno criado

---
+++ demo/src/main/java/com/bn/demo/repositories/PessoaRepository.java
@@ -0,0 +1,7 @@
+package com.bn.demo.repositories;
+
+import com.bn.demo.models.PessoaModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PessoaRepository extends JpaRepository<PessoaModel, Long> {
+}
